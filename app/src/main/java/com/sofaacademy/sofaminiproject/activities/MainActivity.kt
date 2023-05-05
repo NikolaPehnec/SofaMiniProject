783101af199d196bcc65c628b64e2ba745fb0651
nikola <nikola.pehnec12345@gmail.com>
2023-05-05 09:20:29 +0200

    initial

---
+++ app/src/main/java/com/sofaacademy/sofaminiproject/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sofaacademy.sofaminiproject.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sofaacademy.sofaminiproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
